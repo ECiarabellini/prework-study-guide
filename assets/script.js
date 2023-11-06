3b4ca5256ccbf41e520792b277042473086b4679
ECiarabellini <emilyciarabellini@gmail.com>
2023-11-05 16:29:54 -0800

    Add basic for-loop to console.log in js file

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// if (topics === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topics === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+
+for (var x=0; x<topics.length; x++){
+    console.log(topics[x])
+}
